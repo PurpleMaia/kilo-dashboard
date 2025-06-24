61216f4ca11cb63b4d11f1c9b5196a6231dfd87d
Jaden <jaden.kapali@gmail.com>
2025-06-23 14:26:36 -1000

    handle bug when user signs in back with same username (should be able to log in)

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { loginUser, registerUser } from '@/app/lib/auth';
 import { setSessionTokenCookie } from '@/app/lib/session';
+import { db } from '../../../../db/kysely/client';
 
 export async function POST(request: Request) {
   const form = await request.formData();
@@ -8,30 +9,39 @@ export async function POST(request: Request) {
   const password = form.get('password') as string;
 
   try {
+    // check if user exists
+    const existingUser = await db
+      .selectFrom('user')
+      .selectAll()
+      .where('username', '=', username)
+      .executeTakeFirst()
+
     // Try to login, or register if user doesn't exist
     let user, token;
-    try {
-      console.log('trying to login...');
+
+    if (existingUser) {
+      console.log('user exists... attempting login');
       ({ user, token } = await loginUser(username, password));
-    } catch (e) {
+    } else {
       // If login fails, try to register
       console.log('no existing user... signing up user with example.com')
       const email = `${username}@example.com`; // Or get from form if you have it
       await registerUser(username, email, password);
       ({ user, token } = await loginUser(username, password));
     }
-  
     // Set session cookie
     const { origin } = new URL(request.url)
     const response = NextResponse.redirect(`${origin}/dashboard`);
     setSessionTokenCookie(token);
     console.log(user, token)
     return response;
-  } catch (error) {
-    console.error('Login/Signup Error:', error);
-    return NextResponse.json(
-      { error: 'Login or registration failed.' },
-      { status: 400 }
-    );
-  }
+
+    } catch (error) {
+      console.error('Login/Signup Error:', error);
+      return NextResponse.json(
+        { error: 'Login or registration failed.' },
+        { status: 400 }
+      );
+    }
+  
 }
