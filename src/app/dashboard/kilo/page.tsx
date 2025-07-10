3b16a1957d9b9f430d1ada33f3eaadd9d40df23d
Jaden <jaden.kapali@gmail.com>
2025-07-09 16:02:17 -1000

    made kilo page under construction for now

--- src/app/dashboard/kilo/page.tsx
+++ src/app/dashboard/kilo/page.tsx
@@ -1,11 +1,16 @@
 // import GoogleForm from "@/app/ui/googleform"
-import KiloForm from "@/app/components/kilo/KiloForm"
+// import KiloForm from "@/app/components/kilo/KiloForm"
 
 export default function Page() {
     return (
         <>
         {/* <GoogleForm></GoogleForm> */}
-        <KiloForm />
+        {/* <KiloForm /> */}
+        <div className="md:mt-96 mt-96 flex items-center justify-center">
+            <div className="text-center">            
+                <h2 className="text-2xl font-semibold text-gray-700 mb-4">Stay Tuned!</h2>
+            </div>
+        </div>
         </>
     )
 }
