5100b9d262354406ea955d35449ddc3f655a3203
Jaden <jaden.kapali@gmail.com>
2025-07-10 14:41:11 -1000

    made an api call to get the data needed (in case in future)

--- src/app/dashboard/chat/components/ExportData.tsx
+++ src/app/dashboard/chat/components/ExportData.tsx
@@ -1,16 +1,32 @@
+'use client'
 import { Button } from "@/app/ui/button";
 import { FileUp } from "lucide-react";
+import { useState } from "react";
 
 export default function ExportDataButton() {
+    const [exporting, setExporting] = useState<boolean>(false)
 
-    
+    const handleExport = () => {
+        try {
+            setExporting(true)
+
+            
+
+
+
+        } catch {
+
+        } finally {
+            setExporting(false)
+        }
+    }        
     return (
         <>
-            <Button variant={"outline"} className="bg-green-600 text-white font-bold bordershadow-md"
-                
+            <Button variant={"outline"} className="bg-green-600 text-white font-bold bordershadow-md"   
+                onClick={handleExport}             
             >
                 <FileUp className="!w-6 !h-6"/> Export Data
-            </Button>
+            </Button>            
         </>
     )
 }
