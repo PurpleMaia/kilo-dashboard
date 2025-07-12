ec2fff11455e12c60458b14c18cd2c5426b31792
Jaden <jaden.kapali@gmail.com>
2025-07-11 15:27:36 -1000

    linting...

--- src/app/lib/links.ts
+++ src/app/lib/links.ts
@@ -1,5 +1,5 @@
 import { ArrowUpTrayIcon } from '@heroicons/react/24/outline';
-import { Activity, BotMessageSquare, Eye, FileText, LayoutDashboard } from 'lucide-react';
+import { Activity, BotMessageSquare, FileText, LayoutDashboard } from 'lucide-react';
 
 export const links = [
   {
