40e784fe50389a5218f4e6859cfc0efeb3e8190d
Jaden <jaden.kapali@gmail.com>
2025-08-04 15:07:38 -1000

    error handling for no selected 'aina on the dashboard page

--- src/app/components/home/LocationWidgetWrapper.tsx
+++ src/app/components/home/LocationWidgetWrapper.tsx
@@ -6,6 +6,7 @@ import LocationWidget from "./LocationWidget";
 export default function LocationWidgetWrapper() {
   const [loading, setLoading] = useState<boolean>(false)
   const [locations, setLocations] = useState<MalaData[]>([])
+  const [error, setError] = useState<string | null>(null)
 
     useEffect(() => {
       const fetchData = async () => {
@@ -13,10 +14,14 @@ export default function LocationWidgetWrapper() {
           setLoading(true)
           const res = await fetch('api/metrics')
           const data = await res.json()
-          setLocations(data.locations)          
+          setLocations(data.locations)   
+          setError(data.error || null)     
         } catch (e) {
           console.log(e)
-          throw new Error('Error fetching location data')          
+          if (e instanceof Error) {
+            const errorMessage = e.message
+            setError(errorMessage)
+          }
         } finally {
           setLoading(false)
         }
@@ -29,7 +34,15 @@ export default function LocationWidgetWrapper() {
       { loading ? (
         <div className="p-4 rounded-lg bg-white border border-gray-300 shadow-md">Loading...</div>
       ) : (
-        <LocationWidget locations={locations} />
+        <>
+          {error ? (
+            <div className="p-4 rounded-lg bg-yellow-100 border border-yellow-300 text-yellow-700">
+              {error}
+            </div>
+          ) : (
+           <LocationWidget locations={locations} />
+          )}
+        </>
       )}    
     </>
   )
